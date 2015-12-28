96b336d6ace306941d4aafe87682bad1e4dc61e3
Kamunist56 <Kamunist.91@mail.ru>
2015-12-28 13:55:41 +0500

    create table Settings

--- DownMailTest/BaseCreater.cs
+++ DownMailTest/BaseCreater.cs
@@ -23,16 +23,21 @@ namespace DownMailTest
             WorkSQLite work = new WorkSQLite(pathBase);
 
             work.ExecuteQuery("CREATE TABLE" + Func.AddQout("Messages (") +
-                               Func.AddQout("id") + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ," +
-                               Func.AddQout("Subject") + " VARCHAR," + Func.AddQout("From_") + " VARCHAR, " +
-                               Func.AddQout("Data") + " DATETIME," + Func.AddQout("idMessage") + " VARCHAR)");
+                                  Func.AddQout("id") + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ," +
+                                  Func.AddQout("Subject") + " VARCHAR," + Func.AddQout("From_") + " VARCHAR, " +
+                                  Func.AddQout("Data") + " DATETIME," + Func.AddQout("idMessage") + " VARCHAR)");
 
             work.ExecuteQuery("CREATE TABLE " + Func.AddQout("Hosts") + "(" 
-                    + Func.AddQout("id") + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ," 
-                    + Func.AddQout("login") + " VARCHAR, "
-                    + Func.AddQout("pass") + " VARCHAR, "
-                    + Func.AddQout("port") +" INTEGER, "
-                    + Func.AddQout("host") + " VARCHAR)");
+                                + Func.AddQout("id") + " INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ," 
+                                + Func.AddQout("login") + " VARCHAR, "
+                                + Func.AddQout("pass") + " VARCHAR, "
+                                + Func.AddQout("port") +" INTEGER, "
+                                + Func.AddQout("host") + " VARCHAR)");
+
+            work.ExecuteQuery("CREATE TABLE "+ Func.AddQout("Settings")+"("
+                                + Func.AddQout("id")+" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL  UNIQUE ,"
+                                + Func.AddQout("path")+" VARCHAR, "
+                                + Func.AddQout("interval")+" VARCHAR)");
 
         }
     }
